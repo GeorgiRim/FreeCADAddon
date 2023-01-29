7b633e9a09bdf53f40ee53a9b230ab12411c28c3
GeorgiRim <rimargeorg40@gmail.com>
2023-01-30 02:04:20 +0500

    1

---
+++ Models.py
@@ -0,0 +1,13 @@
+class FireSensor:
+
+    def __int__(self, obj):
+        obj.addProperty("")
+        obj.Proxy = self
+
+    def onChanged(self, fp, prop):
+        '''Do something when a property has changed'''
+        FreeCAD.Console.PrintMessage("Change property: " + str(prop) + "\n")
+
+    def execute(self, fp):
+        '''Do something when doing a recomputation, this method is mandatory'''
+        FreeCAD.Console.PrintMessage("Recompute Python Box feature\n")
---
+++ MyModuleA.py
@@ -0,0 +1,26 @@
+import FreeCADGui as Gui
+import FreeCAD as App
+import Image, ImageGui
+import Part, PartGui
+class My_Command_ClassA():
+    """My new command"""
+
+    def GetResources(self):
+        return {"Pixmap"  : "My_Command_Icon", # the name of a svg file available in the resources
+                "Accel"   : "Shift+S", # a default shortcut (optional)
+                "MenuText": "My New Command",
+                "ToolTip" : "What my new command does"}
+
+    def Activated(self):
+        """Do something here"""
+        doc = App.activeDocument()
+        App.Console.PrintMessage('Hello, World!')
+        img_plane = doc.addObject('Image::ImagePlane', 'ImagePlane')
+        return
+
+    def IsActive(self):
+        """Here you can define if the command must be active or not (greyed) if certain conditions
+        are met or not. This function is optional."""
+        return True
+
+Gui.addCommand("My_CommandA", My_Command_ClassA())
